9fd697318c3884adf297be0e5a442b6d5a1bab21
Olga Blud <olga.blud@gmail.com>
2024-07-17 22:39:59 +0300

    update

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+// src/components/App.jsx
+
+import { Product } from "./Product";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Best selling</h1>
+
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
+    </div>
+  );
+}
---
+++ src/components/Product.jsx
@@ -0,0 +1,11 @@
+export const Product = ({ name, imgUrl, price }) => {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="480" />
+      <p>Price: {price} credits</p>
+    </div>
+  );
+};
+
+// export default Product;
